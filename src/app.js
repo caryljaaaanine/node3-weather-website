9a12f88f98b68dfd02a8ade1b0e03bb810ab10ac
Doblon <caryl.janine.doblon@accenture.com>
2021-07-30 15:29:35 +0800

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const forecast = require("./utils/forecast");
 //const { resolveSoa } = require("dns");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // define paths for express config
 const publicDirectoryPath = path.join(__dirname, "../public");
@@ -109,6 +110,6 @@ app.get("*", (req, res) => {
     errorMessage: "Page not found.",
   });
 });
-app.listen(3000, () => {
-  console.log("Server is up on port 3000");
+app.listen(port, () => {
+  console.log("Server is up on port " + port);
 });
